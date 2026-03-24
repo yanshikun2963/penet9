b4cb55aec6269c562a3c512d92e8d27a21b4b31b
yanshikun2963 <yanshikun2963@users.noreply.github.com>
2026-03-24 03:42:40 +0000

    Fix model_zoo.py: _download_url_to_file renamed in PyTorch 2.7+
    torch.hub._download_url_to_file was renamed to download_url_to_file.
    Also fix fallback: urlparse from urllib.parse, HASH_REGEX via re.

--- maskrcnn_benchmark/utils/model_zoo.py
+++ maskrcnn_benchmark/utils/model_zoo.py
@@ -3,13 +3,14 @@ import os
 import sys
 
 try:
-    from torch.hub import _download_url_to_file
+    from torch.hub import download_url_to_file as _download_url_to_file
     from torch.hub import urlparse
     from torch.hub import HASH_REGEX
 except ImportError:
-    from torch.utils.model_zoo import _download_url_to_file
-    from torch.utils.model_zoo import urlparse
-    from torch.utils.model_zoo import HASH_REGEX
+    from torch.hub import download_url_to_file as _download_url_to_file
+    from urllib.parse import urlparse
+    import re
+    HASH_REGEX = re.compile(r'-([a-f0-9]*)\.')
 
 from maskrcnn_benchmark.utils.comm import is_main_process
 from maskrcnn_benchmark.utils.comm import synchronize
